15135a008d2c86d89ce2cb84a14585e0da286cb9
PockyMaster <tsaok@uw.edu>
2012-10-20 03:42:08 -0700

    saslakdjf

--- scoring/models.py
+++ scoring/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.forms import ModelForm
-from grants import GrantApplication
-from fund import Membership
+from grant.models import GrantApplication
+from fund.models import Membership
 
 """Early sketch of a model & modelform for an individual rating.  Feel free to modify or start from scratch 
     
@@ -9,6 +9,9 @@ class ApplicationRating(models.Model):
 
   application = models.ForeignKey(GrantApplication)
   membership = models.ForeignKey(Membership)
+  """added stuff
+  submitted = models.BooleanField(default=False)
+  in_progress = models.BooleanField(default=True)
   
   RATING_CHOICES = (
     (0, 0),
@@ -27,7 +30,10 @@ class ApplicationRating(models.Model):
   submission_time = models.DateTimeField(auto_now_add=True)
   
   def total(self):
-    return (self.program*7+self.diversity*5+self.soundness*4+self.lack_of_access*2+self.collaboration*2)
+	if scoring_bonus_poc or scoring_bonus_geo:
+		return (1.1*self.program*7+self.diversity*5+self.soundness*4+self.lack_of_access*2+self.collaboration*2)
+	}
+	return (self.program*7+self.diversity*5+self.soundness*4+self.lack_of_access*2+self.collaboration*2)
 
 class RatingForm(ModelForm):
   class Meta:
